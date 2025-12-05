2157bda1ce6a9d0b79fdb5c49fdc15c507f916cb
ErickMelendez25 <a20213712@pucp.edu.pe>
2025-12-05 10:43:25 -0500

    configurar urls

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+// Siempre usar solo la env, sin fallback.
+// En desarrollo usarás un archivo .env.development
+// En producción, un archivo .env.production
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
@@ -8,6 +11,7 @@ const axiosClient = axios.create({
   withCredentials: true,
 });
 
+// Interceptor de token
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
