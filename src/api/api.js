88e7f8efdafc60c2323a1fab7f80c0e257c4656c
ErickMelendez25 <a20213712@pucp.edu.pe>
2026-01-24 12:14:34 -0500

    add baseurl del front

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://inventario-quantum-production.up.railway.app"
 });
 
 api.interceptors.request.use(config => {
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://inventario-quantum-production.up.railway.app"
 });
 
 axiosClient.interceptors.request.use(
