f6bfa386bcfc5434f4be65a25579195a89ff0ca6
ErickMelendez25 <a20213712@pucp.edu.pe>
2026-01-27 10:52:55 -0500

    Frontend inicial

--- backend/tests/generar_hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const password = "123456"; // aquí escribe la contraseña real que quieres usar
-
-(async () => {
-  const hash = await bcrypt.hash(password, 10);
-  console.log("NUEVO HASH =>", hash);
-})();
